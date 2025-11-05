2248ec66712417fc146f2df59415a9ac6593aef5
Aleksey Semyonov <semion0ffal@yandex.ru>
2025-11-05 20:49:08 +0300

    Решена 1 задача на массив, приступил ко 2-ой

---
+++ Median of Two Sorted Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Median_of_Two_Sorted_Arrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums1 = { 1, 2 };
+        int[] nums2 = { 3, 4 };
+        Solution solution = new Solution();
+        Console.WriteLine(solution.FindMedianSortedArrays(nums1, nums2));
+    }
+}
+
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        return 0;
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TwoSum;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+internal class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new[] { i, j };
+                }
+            }
+        }
+        return new[] { -1, -1};
+    }
+}
