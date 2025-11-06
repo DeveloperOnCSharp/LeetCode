554104801e0108de9c983c8e11699ea3fb6c4c51
Aleksey Semyonov <semion0ffal@yandex.ru>
2025-11-06 13:27:14 +0300

    Добавлена 2-ая задача и тестировочный solution

--- Median of Two Sorted Arrays/Program.cs
+++ Median of Two Sorted Arrays/Program.cs
@@ -1,20 +1,21 @@
 ﻿namespace Median_of_Two_Sorted_Arrays;
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
-        int[] nums1 = { 1, 2 };
-        int[] nums2 = { 3, 4 };
-        Solution solution = new Solution();
-        Console.WriteLine(solution.FindMedianSortedArrays(nums1, nums2));
+        int[] nums1 = {1, 2, 5, 6, 12};
+        int[] nums2 = {3, 4, 7, 8};
+        int[] newArray = new int[nums1.Length + nums2.Length];
+        
+        //TODO: Вызов функции для заполнения массива
+        
+        //TODO: Поиск медианы и вывод в консоль 
+    }
+    public static double FindMedianSortedArrays(int[] nums1, int[] nums2, ref int[] newArray) 
+    {
+        //TODO: 
     }
 }
 
-public class Solution
-{
-    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
-    {
-        return 0;
-    }
-}
+
---
+++ TEST/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TEST_EXPLORER;
+/*
+ * TODO: тестирования функционала C#
+ */
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int nums = Factorial(5);
+        Console.WriteLine(nums);
+    }
+    static int Factorial(int x)
+    {
+        if (x == 1) return x;
+        return x * Factorial(x - 1);
+    }
+}
+
