bed261faaf4ae59c9b1de036f17f171a7dc790b7
Aleksey Semyonov <semion0ffal@yandex.ru>
2025-11-09 21:45:05 +0300

    Сортировка по папкам

---
+++ Container With Most Water/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Container_With_Most_Water;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [] numbers = [1,8,6,2,5,4,8,3,7];
+        Console.WriteLine(FindSquare(numbers));
+    }
+    static int FindSquare(int[] numbers)
+    {
+        int firstPointer;
+        int secondPointer;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            
+        }
+        return 0;
+    }
+}
--- TEST/Program.cs
+++ TEST/Program.cs
@@ -6,12 +6,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        int[] numbers = new int [10];
+        var random = new Random();
+        foreach (var m in numbers)
+        {
+            numbers[m] = random.Next(0,10);
+            Console.WriteLine(numbers[m]);
+        }
     }
-    static int Factorial(int x)
-    {
-        if (x == 1) return x;
-        return x * Factorial(x - 1);
-    }
+    
 }
 
